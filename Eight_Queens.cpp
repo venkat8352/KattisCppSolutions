c046bfbc103c0222b427755a0e92322c8b08735a
venkat8352 <teja.b8352@gmail.com>
2025-07-24 18:30:02 +0530

    Update Eight_Queens.cpp

--- Eight_Queens.cpp
+++ Eight_Queens.cpp
@@ -10,6 +10,15 @@ int main(){
   bool invalid = false;
   char b[8][8];
   int cnt = 0;
+  //   0 1 2 3 4 5 6 7
+  // 0 . * . . . . . . 
+  // 1 . 
+  // 2 *
+  // 3 .
+  // 4 .
+  // 5 .
+  // 6 .
+  // 7 .
   for(int r = 0; r < 8; r++) {
     for(int c = 0; c < 8; c++) {
       cin >> b[r][c];
@@ -20,37 +29,40 @@ int main(){
     cout << "invalid" << endl;
     return 0;
   }
-  for(int r = 0; r < 8; r++) {
-    for(int c = 0; c < 8; c++) {
+  for(int r = 0; r <= 7; r++) {
+    for(int c = 0; c <= 7; c++) {
       if(b[r][c] == '*') { //a queen
-        //search in that row
-        for(int j = 0; j < 8; j++) {
+
+        //search in the row
+        for(int j = 0; j <= 7; j++) {
           if((j != c) && (b[r][j] == '*')) invalid = true;
         }
-        //search in that column
-        for(int i = 0; i < 8; i++) {
+
+        //search in the column
+        for(int i = 0; i <= 7; i++) {
           if((i != r) && (b[i][c] == '*')) invalid = true;
         }
+
         //search in the left_up to right_down diagonal
         //down dir
-        for(int i = r+1, j = c+1; (i<8)&&(j<8); i++, j++) {
+        for(int i = r+1, j = c+1; (i<=7) && (j<=7); i++, j++) {
           if(b[i][j] == '*') invalid = true;
         }
         //up dir
-        for(int i = r-1, j = c-1; (i>=0)&&(j>=0); i--, j--) {
+        for(int i = r-1, j = c-1; (i>=0) && (j>=0); i--, j--) {
           if(b[i][j] == '*') invalid = true;
         }
 
         //search in the left_down to right_up diagonal
         //down dir
-        for(int i = r-1, j = c+1; (i>=0) && (j<8); i--, j++) {
+        for(int i = r-1, j = c+1; (i>=0) && (j<=7); i--, j++) {
+          if(b[i][j] == '*') invalid = true;
+        }
+        //up dir
+        for(int i = r+1, j = c-1; (i<=7) && (j>=0); i++, j--) {
           if(b[i][j] == '*') invalid = true;
         }
 
-        //up dir
-        for(int i = r+1, j = c-1; (i<8) && (j>=0); i++, j--) {
-          if(b[i][j] == '*') invalid = true;
-        }
       }
       if(invalid) break;
     }
